d53916b29487fb068bcbdaa537b228822bdf8a9b
jpsturgell <jpsturgell@wustl.edu>
2023-02-21 21:40:39 -0600

    Options and Level Select Implemented

--- inertia/Assets/Code/Mind.cs
+++ inertia/Assets/Code/Mind.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewGameButton : MonoBehaviour
+public class Mind : MonoBehaviour
 {
     public int newGameScene;
 
